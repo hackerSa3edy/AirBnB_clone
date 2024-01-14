13dd18144f1f77c1a3cc12b0aabde3a8d703223a
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-01-14 12:15:22 +0200

    Add more documentation

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Amenity Class"""
+"""Amenity Module to create Amenities"""
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Initialize Amenity class"""
+    """Amenity class
+
+    Arguments:
+        BaseModel -- Inherts BaseModel's attributes and methods
+    """
 
     name = ''
