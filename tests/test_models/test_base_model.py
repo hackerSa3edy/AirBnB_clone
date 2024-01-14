d0b2a88840036a98ba18c394fad0410d0aa8c7a1
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-01-15 01:22:04 +0200

    Add documentation for the test_base_model.py module

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Unittests Module for testing the BaseModel class"""
+"""
+This module provides a unittest suite for testing the BaseModel class. The BaseModel class serves as a foundation for other classes and is intended to handle common attributes and behaviors shared by various objects in a Python application.
+"""
 import unittest
 import os
 import datetime
@@ -10,8 +12,18 @@ import json
 
 
 class TestBaseModelInit(unittest.TestCase):
+    """Test the constructor of the BaseModel class
+
+    Arguments:
+        unittest -- Inherits unittest.TestCase's attributes and methods
+    """
 
     def setUp(self):
+        """Rename the storage file, so it doesn't get overwrited
+
+        Returns:
+            The default behavior of the parent class
+        """
         try:
             os.rename('file.json', 'temp')
         except FileNotFoundError:
@@ -20,6 +32,11 @@ class TestBaseModelInit(unittest.TestCase):
         return super().setUp()
 
     def tearDown(self):
+        """Reset the storage file name to its default
+
+        Returns:
+            The default behavior of the parent class
+        """
         try:
             os.remove('file.json')
         except FileNotFoundError:
@@ -33,12 +50,16 @@ class TestBaseModelInit(unittest.TestCase):
         return super().tearDown()
 
     def test_public_attributes(self):
+        """Test if the attributes are public
+        """
         obj = BaseModel()
         self.assertEqual(str, type(obj.id))
         self.assertEqual(datetime.datetime, type(obj.created_at))
         self.assertEqual(datetime.datetime, type(obj.updated_at))
 
     def test_uniq_id_for_multiple_objects(self):
+        """Test the uniqueness of objects' ids
+        """
         obj1 = BaseModel()
         obj2 = BaseModel()
         obj3 = BaseModel()
@@ -47,14 +68,20 @@ class TestBaseModelInit(unittest.TestCase):
         self.assertTrue(len(ids) == 4)
 
     def test_same_createdAt_updatedAt(self):
+        """Test if created_at and updated_at are the same
+        """
         obj = BaseModel()
         self.assertEqual(obj.created_at, obj.updated_at)
 
     def test_obj_existance_in_storage(self):
+        """Test if the created object exists in the storage
+        """
         obj = BaseModel()
         self.assertIn(obj, models.storage.all().values())
 
     def test_instance_recreate_from_dict(self):
+        """Test recreating an instance from a dictionary
+        """
         date = datetime.datetime(2024, 1, 14, 17, 7, 0, 0).isoformat()
         dict1 = {
             'id': '1809',
@@ -67,15 +94,21 @@ class TestBaseModelInit(unittest.TestCase):
         self.assertDictEqual(dict1, dict2)
 
     def test_unused_args(self):
+        """Test creating an object with unused arguments
+        """
         obj = BaseModel('1809')
         self.assertNotIn('1809', obj.__dict__.values())
 
     def test_unused_args_and_used_kwargs(self):
+        """Tests the creation of an object with unused arguments and used keyword arguments.
+        """
         obj = BaseModel('1809', year='2024')
         self.assertNotIn('1809', obj.__dict__.values())
         self.assertIn('2024', obj.__dict__.values())
 
     def test_adding_additional_attributes(self):
+        """Tests adding additional attributes to the object.
+        """
         obj = BaseModel()
         obj.name = 'Abdelrahman'
         obj.code = 7
@@ -92,6 +125,8 @@ class TestBaseModelInit(unittest.TestCase):
         self.assertEqual(obj.available, True)
 
     def test_createdAt_typeError(self):
+        """Tests TypeError when creating an object with an incorrect data type for created_at.
+        """
         dict1 = {
             'id': '1809',
             'created_at': '1809',
@@ -102,6 +137,8 @@ class TestBaseModelInit(unittest.TestCase):
             BaseModel(**dict1)
 
     def test_updatedAt_typeError(self):
+        """Tests TypeError when creating an object with an incorrect data type for updated_at.
+        """
         date = datetime.datetime(2024, 1, 14, 17, 7, 0, 0)
         dict1 = {
             'id': '1809',
@@ -113,12 +150,21 @@ class TestBaseModelInit(unittest.TestCase):
             BaseModel(**dict1)
 
     def test_with_None_kwargs(self):
+        """Tests TypeError when creating an object with None as arguments.
+        """
         with self.assertRaises(TypeError):
             BaseModel(id=None, created_at=None, updated_at=None)
 
 
 class TestBaseModelStr(unittest.TestCase):
+    """This class contains test cases for the __str__ method of the BaseModel class.
+    """
     def setUp(self):
+        """Rename the storage file, so it doesn't get overwrited
+
+        Returns:
+            The default behavior of the parent class
+        """
         try:
             os.rename('file.json', 'temp')
         except FileNotFoundError:
@@ -127,6 +173,11 @@ class TestBaseModelStr(unittest.TestCase):
         return super().setUp()
 
     def tearDown(self):
+        """Reset the storage file name to its default
+
+        Returns:
+            The default behavior of the parent class
+        """
         try:
             os.remove('file.json')
         except FileNotFoundError:
@@ -139,7 +190,10 @@ class TestBaseModelStr(unittest.TestCase):
 
         return super().tearDown()
 
+
     def test_formated_output(self):
+        """Tests if the output of the __str__ method is formatted correctly.
+        """
         obj = BaseModel()
         obj.id = '1809'
         obj.created_at = datetime.datetime(2024, 1, 14, 19, 45, 3, 255968)
@@ -154,7 +208,14 @@ class TestBaseModelStr(unittest.TestCase):
 
 
 class TestBaseModelSave(unittest.TestCase):
+    """This class contains test cases for the save method of the BaseModel class.
+    """
     def setUp(self):
+        """Rename the storage file, so it doesn't get overwrited
+
+        Returns:
+            The default behavior of the parent class
+        """
         try:
             os.rename('file.json', 'temp')
         except FileNotFoundError:
@@ -163,6 +224,11 @@ class TestBaseModelSave(unittest.TestCase):
         return super().setUp()
 
     def tearDown(self):
+        """Reset the storage file name to its default
+
+        Returns:
+            The default behavior of the parent class
+        """
         try:
             os.remove('file.json')
         except FileNotFoundError:
@@ -175,30 +241,41 @@ class TestBaseModelSave(unittest.TestCase):
 
         return super().tearDown()
 
+
     def test_save_with_args(self):
+        """Tests if save method raises a TypeError when called with arguments.
+        """
         obj = BaseModel()
         with self.assertRaises(TypeError):
             obj.save('arg')
 
     def test_updatedAt_greaterThan_createdAt(self):
+        """Tests if updated_at is greater than created_at after calling save.
+        """
         obj = BaseModel()
         sleep(0.3)
         obj.save()
         self.assertGreater(obj.updated_at, obj.created_at)
 
     def test_updatedAt_greaterThan_prev_updatedAt(self):
+        """Tests if updated_at is greater than the previous updated_at after calling save.
+        """
         obj = BaseModel()
         prev_updated = obj.updated_at
         obj.save()
         self.assertGreater(obj.updated_at, prev_updated)
 
     def test_updatedAt_type(self):
+        """Tests if updated_at retains its type after calling save.
+        """
         obj = BaseModel()
         obj.updated_at = 'foo'
         obj.save()
         self.assertEqual(datetime.datetime, type(obj.updated_at))
 
     def test_saved_obj_in_storage(self):
+        """Tests if the saved object exists in the storage file.
+        """
         obj = BaseModel()
         obj.save()
         with open('file.json', 'r') as f:
@@ -207,7 +284,14 @@ class TestBaseModelSave(unittest.TestCase):
 
 
 class TestBaseModelToDict(unittest.TestCase):
+    """This class contains test cases for the to_dict method of the BaseModel class.
+    """
     def setUp(self):
+        """Rename the storage file, so it doesn't get overwrited
+
+        Returns:
+            The default behavior of the parent class
+        """
         try:
             os.rename('file.json', 'temp')
         except FileNotFoundError:
@@ -216,6 +300,11 @@ class TestBaseModelToDict(unittest.TestCase):
         return super().setUp()
 
     def tearDown(self):
+        """Reset the storage file name to its default
+
+        Returns:
+            The default behavior of the parent class
+        """
         try:
             os.remove('file.json')
         except FileNotFoundError:
@@ -229,11 +318,15 @@ class TestBaseModelToDict(unittest.TestCase):
         return super().tearDown()
 
     def test_to_dict_with_args(self):
+        """Tests if to_dict method raises a TypeError when called with arguments.
+        """
         obj = BaseModel()
         with self.assertRaises(TypeError):
             obj.to_dict('arg')
 
     def test_create_instance_from_dict(self):
+        """Tests if an object can be created from a dictionary.
+        """
         obj = BaseModel()
         data = obj.to_dict()
         obj2 = BaseModel(**data)
@@ -242,17 +335,23 @@ class TestBaseModelToDict(unittest.TestCase):
         self.assertEqual(obj.updated_at, obj2.updated_at)
 
     def test_return_type(self):
+        """Tests if the return type of to_dict is a dictionary.
+        """
         obj = BaseModel()
         data = obj.to_dict()
         self.assertEqual(dict, type(data))
 
     def test_dict_keys_types(self):
+        """Tests if all keys in the dictionary returned by to_dict have the correct type.
+        """
         obj = BaseModel()
         data = obj.to_dict()
         for key in data.keys():
             self.assertEqual(str, type(key))
 
     def test_dict_keys(self):
+        """Tests if the keys in the dictionary match the expected keys.
+        """
         obj = BaseModel()
         data = list(obj.to_dict().keys())
         keys = ['id', 'created_at', 'updated_at', '__class__']
@@ -261,12 +360,16 @@ class TestBaseModelToDict(unittest.TestCase):
         self.assertEqual(data, keys)
 
     def test_dict_values_types(self):
+        """Tests if all values in the dictionary returned by to_dict have the correct type.
+        """
         obj = BaseModel()
         data = obj.to_dict()
         for value in data.values():
             self.assertEqual(str, type(value))
 
     def test_dict_values(self):
+        """Tests if the values in the dictionary match the expected values.
+        """
         date = datetime.datetime(2024, 1, 14, 17, 7, 0, 0)
         dict1 = {
             'id': '1809',
