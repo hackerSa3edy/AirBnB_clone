6ae3e4475d9ee5589f7a140c8b404e1626433202
Z-Sitawi <aaichaou.zakaria@gmail.com>
2024-01-14 00:17:00 -0800

    the 'fix consol output 101

--- console.py
+++ console.py
@@ -108,10 +108,10 @@ class HBNBCommand(cmd.Cmd):
             for key, val in storage.all().items():
                 if model in key:
                     list_instances.append(str(val))
-            if not list_instances:
-                return
-            else:
-                print(list_instances)
+        if not list_instances:
+            return
+        else:
+            print(list_instances)
 
     def do_update(self, args):
         """Updates an instance based on the class name and id.
