215be8cd289e698d0c5769e3962636ed9d6b98d0
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-01-16 14:44:20 +0200

    Add unittests for fileStorage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,9 +8,11 @@ various test classes, each focusing on specific aspects of the FileStorage
 class.
 """
 import unittest
+import datetime
+import json
+import os
 from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-
+import models
 
 class TestFileStorageAttrs(unittest.TestCase):
     """Testing the attributes of the FileStorage class.
@@ -18,20 +20,58 @@ class TestFileStorageAttrs(unittest.TestCase):
     Arguments:
         unittest -- Inherits unittest.TestCase's attributes and methods
     """
-    def test_private_attr_file_path(self):
+    def setUp(self):
+        """Rename the storage file, so it doesn't get overwrited
+
+        Returns:
+            The default behavior of the parent class
+        """
+        try:
+            os.rename('file.json', 'temp')
+        except FileNotFoundError:
+            pass
+
+        # Initialize new storage before each test
+        models.FileStorage._FileStorage__objects = {}
+
+        return super().setUp()
+
+    def tearDown(self):
+        """Reset the storage file name to its default
+
+        Returns:
+            The default behavior of the parent class
+        """
+        try:
+            os.remove('file.json')
+        except FileNotFoundError:
+            pass
+
+        try:
+            os.rename('temp', 'file.json')
+        except FileNotFoundError:
+            pass
+
+        return super().tearDown()
+
+    def test_private_attr_filePath(self):
         """Tests the existence and privacy of the __file_path attribute.
         """
-        pass
+        with self.assertRaises(AttributeError):
+            models.storage.__file_path
+
 
     def test_private_attr_objects(self):
         """Tests the existence and privacy of the __objects attribute.
         """
-        pass
+        with self.assertRaises(AttributeError):
+            models.storage.__objects
 
     def test_with_additional_attr(self):
         """Test adding additional attributes
         """
-        pass
+        models.storage.test = True
+        self.assertEqual(models.storage.test, True)
 
 
 class TestFileStorageAll(unittest.TestCase):
@@ -41,15 +81,59 @@ class TestFileStorageAll(unittest.TestCase):
         unittest -- _description_Inherits unittest.TestCase's attributes
         and methods
     """
+
+    def setUp(self):
+        """Rename the storage file, so it doesn't get overwrited
+
+        Returns:
+            The default behavior of the parent class
+        """
+        try:
+            os.rename('file.json', 'temp')
+        except FileNotFoundError:
+            pass
+
+        # Initialize new storage before each test
+        models.FileStorage._FileStorage__objects = {}
+
+        return super().setUp()
+
+    def tearDown(self):
+        """Reset the storage file name to its default
+
+        Returns:
+            The default behavior of the parent class
+        """
+        try:
+            os.remove('file.json')
+        except FileNotFoundError:
+            pass
+
+        try:
+            os.rename('temp', 'file.json')
+        except FileNotFoundError:
+            pass
+
+        return super().tearDown()
+
     def test_objects_same_as_in_objects_attr(self):
         """Ensures that loaded objects is the same as the object.
         """
-        pass
+        obj = BaseModel()
+        obj2 = BaseModel()
+        self.assertDictEqual(
+            {
+                f"BaseModel.{obj.id}": obj,
+                f"BaseModel.{obj2.id}": obj2
+                },
+            models.storage.all()
+            )
 
     def test_with_additional_args(self):
         """Test adding additional arguments
         """
-        pass
+        with self.assertRaises(TypeError):
+            models.storage.all('argument')
 
 
 class TestFileStorageNew(unittest.TestCase):
@@ -58,28 +142,74 @@ class TestFileStorageNew(unittest.TestCase):
     Arguments:
         unittest -- Inherits unittest.TestCase's attributes and methods
     """
+
+    def setUp(self):
+        """Rename the storage file, so it doesn't get overwrited
+
+        Returns:
+            The default behavior of the parent class
+        """
+        try:
+            os.rename('file.json', 'temp')
+        except FileNotFoundError:
+            pass
+
+        # Initialize new storage before each test
+        models.FileStorage._FileStorage__objects = {}
+
+        return super().setUp()
+
+    def tearDown(self):
+        """Reset the storage file name to its default
+
+        Returns:
+            The default behavior of the parent class
+        """
+        try:
+            os.remove('file.json')
+        except FileNotFoundError:
+            pass
+
+        try:
+            os.rename('temp', 'file.json')
+        except FileNotFoundError:
+            pass
+
+        return super().tearDown()
+
     def test_new_object_in_objects_attr(self):
         """Verifies that a new object is correctly added to the __objects
         attribute.
         """
-        pass
+        obj = BaseModel()
+        self.assertIn(obj, models.storage.all().values())
 
     def test_new_with_noArgs(self):
         """Tests the behavior of the new method when called with no
         arguments.
         """
-        pass
+        with self.assertRaises(TypeError):
+            models.storage.new()
 
     def test_new_object_without_id_attr(self):
         """Tests the behavior of the new method when called with an object
         without the id attribute.
         """
-        pass
+        date = datetime.datetime(2024, 1, 14, 17, 7, 0, 0)
+        dict1 = {
+            'created_at': date.isoformat(),
+            'updated_at': date.isoformat(),
+            '__class__': 'BaseModel'
+            }
+        obj = BaseModel(**dict1)
+        with self.assertRaises(AttributeError):
+            models.storage.new(obj)
 
     def test_with_additional_args(self):
         """Test adding additional arguments
         """
-        pass
+        with self.assertRaises(TypeError):
+            models.storage.new(BaseModel(), 'argument')
 
 
 class TestFileStorageSave(unittest.TestCase):
@@ -88,31 +218,77 @@ class TestFileStorageSave(unittest.TestCase):
     Arguments:
         unittest -- Inherits unittest.TestCase's attributes and methods
     """
+
+    def setUp(self):
+        """Rename the storage file, so it doesn't get overwrited
+
+        Returns:
+            The default behavior of the parent class
+        """
+        try:
+            os.rename('file.json', 'temp')
+        except FileNotFoundError:
+            pass
+
+        # Initialize new storage before each test
+        models.FileStorage._FileStorage__objects = {}
+
+        return super().setUp()
+
+    def tearDown(self):
+        """Reset the storage file name to its default
+
+        Returns:
+            The default behavior of the parent class
+        """
+        try:
+            os.remove('file.json')
+        except FileNotFoundError:
+            pass
+
+        try:
+            os.rename('temp', 'file.json')
+        except FileNotFoundError:
+            pass
+
+        return super().tearDown()
+
     def test_with_additional_args(self):
         """Test adding additional arguments
         """
-        pass
+        with self.assertRaises(TypeError):
+            models.storage.save('argument')
 
     def test_object_saved_in_file(self):
         """Verifies that objects are correctly saved to the JSON file.
         """
-        pass
+        obj = BaseModel()
+        obj.save()
+        data = obj.to_dict()
+        with open('file.json', 'r') as file:
+            json_data = json.load(file)
 
-    def test_dumped_objects_not_json(self):
-        """Test the behavior of dumping a not json object
-        """
-        pass
+        self.assertIn(data, list(json_data.values()))
 
     def test_object_without_toDict_method(self):
         """Ensures proper handling when attempting to save an object without
         a to_dict method.
         """
-        pass
+        class Test:
+            id = '333'
+        obj = Test()
+        models.storage.new(obj)
+        with self.assertRaises(AttributeError):
+            models.storage.save()
 
     def test_saved_empty_objects_attribute(self):
         """Tests the behavior of saving when the __objects attribute is empty.
         """
-        pass
+        models.storage.save()
+        with open('file.json', 'r') as file:
+            json_data = json.load(file)
+        self.assertDictEqual(json_data, models.storage.all())
+
 
 
 class TestFileStorageReload(unittest.TestCase):
@@ -121,42 +297,118 @@ class TestFileStorageReload(unittest.TestCase):
     Arguments:
         unittest -- Inherits unittest.TestCase's attributes and methods
     """
+
+    def setUp(self):
+        """Rename the storage file, so it doesn't get overwrited
+
+        Returns:
+            The default behavior of the parent class
+        """
+        try:
+            os.rename('file.json', 'temp')
+        except FileNotFoundError:
+            pass
+
+        # Initialize new storage before each test
+        models.FileStorage._FileStorage__objects = {}
+
+        return super().setUp()
+
+    def tearDown(self):
+        """Reset the storage file name to its default
+
+        Returns:
+            The default behavior of the parent class
+        """
+        try:
+            os.remove('file.json')
+        except FileNotFoundError:
+            pass
+
+        try:
+            os.rename('temp', 'file.json')
+        except FileNotFoundError:
+            pass
+
+        return super().tearDown()
+
     def test_with_additional_args(self):
         """Test adding additional arguments
         """
-        pass
+        with self.assertRaises(TypeError):
+            models.storage.reload('argument')
 
     def test_file_not_exist(self):
         """Tests the behavior when attempting to reload from a non-existent
         file.
         """
-        pass
+        obj = BaseModel()
+        prev_dict = models.storage.all()
+        models.storage.reload()
+        curr_dict = models.storage.all()
+        self.assertDictEqual(prev_dict, curr_dict)
 
     def test_loaded_objects_not_json(self):
         """Ensures proper handling when the loaded data from the file is not
         in JSON format.
         """
-        pass
+        with open('file.json', 'w') as file:
+            file.write("Well son, very proud of you.")
+
+        with self.assertRaises(json.JSONDecodeError):
+            models.storage.reload()
 
     def test_specified_object_not_found_in_models_dict(self):
         """Tests the case when a specified object in the loaded data is not
         found in the models_dict.
         """
-        pass
+        with open('file.json', 'w') as file:
+            file.write(\
+                '{"BaseModel.f1a83b88-534e-4cf1-841d-091f71f057af": {' + \
+                    '"id": "f1a83b88-534e-4cf1-841d-091f71f057af",' + \
+                    '"created_at": "2024-01-16T13:51:12.445588",' + \
+                    '"updated_at": "2024-01-16T13:51:16.891133",' + \
+                    '"__class__": "TestReload"'
+                    '}' + \
+                '}')
+
+        with self.assertRaises(KeyError):
+            models.storage.reload()
 
     def test_the_attr_class_not_found_in_object(self):
         """Verifies the handling of cases where the '__class__' attribute
         is not found in the loaded object.
         """
-        pass
+
+        with open('file.json', 'w') as file:
+            file.write(\
+                '{"BaseModel.f1a83b88-534e-4cf1-841d-091f71f057af": {' + \
+                    '"id": "f1a83b88-534e-4cf1-841d-091f71f057af",' + \
+                    '"created_at": "2024-01-16T13:51:12.445588",' + \
+                    '"updated_at": "2024-01-16T13:51:16.891133"' + \
+                    '}' + \
+                '}')
+
+        with self.assertRaises(KeyError) as file:
+            models.storage.reload()
 
     def test_empty_loaded_file(self):
         """Test the behavior when loading an empty file
         """
-        pass
+        models.storage.reload()
+        self.assertDictEqual({}, models.storage.all())
 
-    def test_reloaded_objects_are_in_file(self):
-        """Ensures that the objects reloaded from the file match the
-        contents of the file.
+    def test_new_object_before_saving_exists_after_reload(self):
+        """Ensures that the new objects before saving them in a file
+        are exist after reloading objects from a file.
         """
-        pass
+        obj = BaseModel()
+        obj.save()
+        obj2 = BaseModel()
+        models.storage.reload()
+
+        self.assertIn(obj2, list(models.storage.all().values()))
+
+
+if __name__ == '__main__':
+    unittest.main()
