55f1542a31f832e5f6c3c5ceb32d5985f0d56313
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-01-16 10:53:05 +0200

    Document the module including its classes with its methods

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""_summary_
+"""
+This testing module is designed to thoroughly test the functionalities
+of the FileStorage class, which is responsible for serializing instances
+to a JSON file and deserializing JSON files to instances in the context
+of a simple object-relational mapping (ORM) system. The module includes
+various test classes, each focusing on specific aspects of the FileStorage
+class.
 """
 import unittest
 from models.base_model import BaseModel
@@ -7,130 +13,140 @@ from models.engine.file_storage import FileStorage
 
 
 class TestFileStorageAttrs(unittest.TestCase):
-    """_summary_
+    """Testing the attributes of the FileStorage class.
 
     Arguments:
-        unittest -- _description_
+        unittest -- Inherits unittest.TestCase's attributes and methods
     """
     def test_private_attr_file_path(self):
-        """_summary_
+        """Tests the existence and privacy of the __file_path attribute.
         """
         pass
 
     def test_private_attr_objects(self):
-        """_summary_
+        """Tests the existence and privacy of the __objects attribute.
         """
         pass
 
     def test_with_additional_attr(self):
-        """_summary_
+        """Test adding additional attributes
         """
         pass
 
 
 class TestFileStorageAll(unittest.TestCase):
-    """_summary_
+    """Testing the all method of the FileStorage class.
 
     Arguments:
-        unittest -- _description_
+        unittest -- _description_Inherits unittest.TestCase's attributes and methods
     """
-    def test_objects_existance_at_objects_attr(self):
-        """_summary_
+    def test_objects_same_as_in_objects_attr(self):
+        """Ensures that loaded objects is the same as the object.
         """
         pass
 
-    def test_with_additional_attr(self):
-        """_summary_
+    def test_with_additional_args(self):
+        """Test adding additional arguments
         """
         pass
 
 
 class TestFileStorageNew(unittest.TestCase):
-    """_summary_
+    """Testing the new method of the FileStorage class.
 
     Arguments:
-        unittest -- _description_
+        unittest -- Inherits unittest.TestCase's attributes and methods
     """
     def test_new_object_in_objects_attr(self):
-        """_summary_
+        """Verifies that a new object is correctly added to the __objects attribute.
         """
         pass
 
     def test_new_with_noArgs(self):
-        """_summary_
+        """Tests the behavior of the new method when called with no arguments.
         """
         pass
 
     def test_new_object_without_id_attr(self):
-        """_summary_
+        """Tests the behavior of the new method when called with an object without the id attribute.
         """
         pass
 
-    def test_with_additional_attr(self):
-        """_summary_
+    def test_with_additional_args(self):
+        """Test adding additional arguments
         """
         pass
 
 
 class TestFileStorageSave(unittest.TestCase):
-    """_summary_
+    """Test the save method of the FileStorage class.
 
     Arguments:
-        unittest -- _description_
+        unittest -- Inherits unittest.TestCase's attributes and methods
     """
-    def test_with_additional_attr(self):
-        """_summary_
+    def test_with_additional_args(self):
+        """Test adding additional arguments
         """
         pass
 
     def test_object_saved_in_file(self):
-        """_summary_
+        """Verifies that objects are correctly saved to the JSON file.
+        """
+        pass
+
+    def test_dumped_objects_not_json(self):
+        """Test the behavior of dumping a not json object
         """
         pass
 
     def test_object_without_toDict_method(self):
-        """_summary_
+        """Ensures proper handling when attempting to save an object without a to_dict method.
         """
         pass
 
-    def test_saved_empty_objects_attr(self):
-        """_summary_
+    def test_saved_empty_objects_attribute(self):
+        """Tests the behavior of saving when the __objects attribute is empty.
         """
         pass
 
 
 class TestFileStorageReload(unittest.TestCase):
-    """_summary_
+    """Testing the reload method of the FileStorage class.
 
     Arguments:
-        unittest -- _description_
+        unittest -- Inherits unittest.TestCase's attributes and methods
     """
-    def test_with_additional_attr(self):
-        """_summary_
+    def test_with_additional_args(self):
+        """Test adding additional arguments
         """
         pass
 
     def test_file_not_exist(self):
-        """_summary_
+        """Tests the behavior when attempting to reload from a non-existent file.
         """
         pass
 
     def test_loaded_objects_not_json(self):
-        """_summary_
+        """Ensures proper handling when the loaded data from the file is not in JSON format.
         """
         pass
 
     def test_specified_object_not_found_in_models_dict(self):
-        """_summary_
+        """Tests the case when a specified object in the loaded data is not found in the models_dict.
         """
         pass
 
     def test_the_attr_class_not_found_in_object(self):
-        """_summary_
+        """Verifies the handling of cases where the '__class__' attribute is not found in the loaded object.
+        """
+        pass
+
+    def test_empty_loaded_file(self):
+        """Test the behavior when loading an empty file
         """
         pass
 
     def test_reloaded_objects_are_in_file(self):
-        """_summary_
+        """Ensures that the objects reloaded from the file match the contents of the file.
         """
         pass
